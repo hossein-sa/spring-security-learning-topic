5ea50c55f1e99afbd6fe0edb93473ec36f644df6
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2023-10-07 16:58:49 +0330

    Token entity fields

--- src/main/java/ir/hsadeghi/security/token/Token.java
+++ src/main/java/ir/hsadeghi/security/token/Token.java
@@ -1,4 +1,34 @@
 package ir.hsadeghi.security.token;
 
+import ir.hsadeghi.security.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+
 public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType;
+
+    private boolean expired;
+    private boolean revoked;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
 }
