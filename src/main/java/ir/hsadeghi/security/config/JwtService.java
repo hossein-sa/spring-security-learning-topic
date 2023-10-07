0280e46d5203bb7df0e879a202ed0a98c354edcc
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2023-10-07 14:51:26 +0330

    extract claims methods

--- src/main/java/ir/hsadeghi/security/config/JwtService.java
+++ src/main/java/ir/hsadeghi/security/config/JwtService.java
@@ -1,10 +1,39 @@
 package ir.hsadeghi.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "29a898eedc3bff730347db73626c92fb8f0c32f2549d8bb847a2fd6217e69331";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
