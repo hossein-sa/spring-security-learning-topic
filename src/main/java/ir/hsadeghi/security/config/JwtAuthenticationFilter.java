66cdc07b57c1cf52e3a6ca5f2ecbaed282b4968b
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2023-10-08 15:00:17 +0330

    add log feature and implement expire and revoke token

--- src/main/java/ir/hsadeghi/security/config/JwtAuthenticationFilter.java
+++ src/main/java/ir/hsadeghi/security/config/JwtAuthenticationFilter.java
@@ -1,10 +1,13 @@
 package ir.hsadeghi.security.config;
 
+import ir.hsadeghi.security.token.TokenRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,8 +22,13 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final TokenRepository tokenRepository;
+
 
     @Override
     protected void doFilterInternal(
@@ -28,10 +36,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+        logger.debug("Entering JwtAuthenticationFilter");
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
+
+        logger.debug("Received request to JwtAuthenticationFilter");
+
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            logger.debug("No valid JWT found in the request. Skipping authentication.");
             filterChain.doFilter(request, response);
             return;
         }
@@ -39,7 +52,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userEmail = jwtService.extractUsername(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            if (jwtService.isTokenValid(jwt, userDetails)) {
+            var isTokenValid = tokenRepository.findByToken(jwt)
+                    .map(t -> !t.isExpired() && !t.isRevoked())
+                    .orElse(false);
+            if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
+                logger.debug("JWT token is valid. Creating authentication token.");
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
@@ -49,7 +66,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         new WebAuthenticationDetailsSource().buildDetails(request)
                 );
                 SecurityContextHolder.getContext().setAuthentication(authToken);
+            } else {
+                logger.warn("JWT token is invalid.");
             }
+        } else {
+            logger.debug("User already authenticated or email not found in JWT.");
         }
         filterChain.doFilter(request, response);
     }
