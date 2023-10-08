5894b0df3c5405597a540b4ad073847765afa2d9
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2023-10-08 17:04:23 +0330

    use Annotation PreAuthorize for manage authorities

--- src/main/java/ir/hsadeghi/security/config/SecurityConfiguration.java
+++ src/main/java/ir/hsadeghi/security/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity // replaced by @EnableGlobalMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
