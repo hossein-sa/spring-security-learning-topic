ee71bbc647860303fbdabe2213399337a3fd2ef1
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2023-10-09 16:15:23 +0330

    add swagger configs

--- src/main/java/ir/hsadeghi/security/demo/ManagementController.java
+++ src/main/java/ir/hsadeghi/security/demo/ManagementController.java
@@ -1,11 +1,32 @@
 package ir.hsadeghi.security.demo;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/management")
+@Tag(name = "Management")
 public class ManagementController {
 
+
+    @Operation(
+            description = "Get endpoint for manager",
+            summary = "This is a summary for management get endpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "success",
+                            responseCode = "200"
+
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid token",
+                            responseCode = "403"
+
+                    )
+            }
+    )
     @GetMapping
     public String get(){
         return "GET:: Management controller";
