eb8eff5a1b0bbbccbf2a07595b2640074e4baef0
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2023-10-18 17:19:17 +0330

    add change password

---
+++ src/main/java/ir/hsadeghi/security/user/UserService.java
@@ -0,0 +1,32 @@
+package ir.hsadeghi.security.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository repository;
+
+    public void changePassword(ChangePasswordRequest request, Principal connectedUser) {
+
+        var user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+
+        //check if the current password is correct
+        if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
+            throw new IllegalStateException("Wrong Password");
+        }
+        //check if two password is the same
+        if (!request.getNewPassword().equals(request.getConfirmationPassword())) {
+            throw new IllegalStateException("Password are not the same");
+        }
+        //save the new password and update it
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        repository.save(user);
+    }
+}
